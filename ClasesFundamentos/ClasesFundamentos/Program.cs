df5a638581a1b887dae081b044a54e3e3b764f4d
emilson rodriguez <emilson.rodriguez.freelance@gmail.com>
2021-01-30 10:20:56 -0400

    Agregando codigo

--- ClasesFundamentos/ClasesFundamentos/Program.cs
+++ ClasesFundamentos/ClasesFundamentos/Program.cs
@@ -10,7 +10,15 @@ namespace ClasesFundamentos
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hola Mundo");
+            int numero1 = 2000000;
+            double numeroGrande = numero1; // cuando no necesita un tipo en el casting es un Conversión implicita
+            double numeroG2 = double.Parse("45000000000");// cuando si necesita un tipo en el casting es un Conversión explicita
+            numeroG2 = Convert.ToDouble("85000000000");
+            //numero1 = (int)numeroG2; esta forma de cambio de tipo de dato no es recomendable
+            //numero1 = Convert.ToInt32(numeroG2); //es mejor usar el convert para controlar o conocer errores
+            //numero1 = int.Parse(numeroG2); la funcion Parse funciona mejor con cadenas
+            int numero2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
